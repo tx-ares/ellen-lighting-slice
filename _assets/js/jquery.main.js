67a4e8ece5d721213c7c67d507d020d4ed4f3242
txsadamwest <twinadanz@gmail.com>
2017-03-16 17:08:10 -0500

    fixed slider, need to style

--- _assets/js/jquery.main.js
+++ _assets/js/jquery.main.js
@@ -87,23 +87,51 @@ function initdataImageBackground() {
 }
 
 function initSlider() {
-	"use strict";
-	
-	$('.slider')
-			.slick({
-				lazyLoad: 'ondemand',
-				autoplay: true,
-				autoplaySpeed: 6000,
-				pauseOnHover: false,
-				dots: false,
-				ininite: true
-				
-			});	
+    "use strict";
 
-	
-	
-	$(window).trigger('resize');
-	
+    $('.slider')
+			.slick({
+			    lazyLoad: 'ondemand',
+			    autoplay: true,
+			    autoplaySpeed: 6000,
+			    pauseOnHover: false,
+			    dots: false,
+			    ininite: true,
+			    responsive: [
+   {
+       breakpoint: 1024,
+       settings: {
+           slidesToShow: 3,
+           slidesToScroll: 3,
+           infinite: true,
+           dots: true
+       }
+   },
+   {
+       breakpoint: 600,
+       settings: {
+           slidesToShow: 2,
+           slidesToScroll: 2
+       }
+   },
+   {
+       breakpoint: 480,
+       settings: {
+           slidesToShow: 1,
+           slidesToScroll: 1
+       }
+   }
+   // You can unslick at a given breakpoint now by adding:
+   // settings: "unslick"
+   // instead of a settings object
+			    ]
+
+			});
+
+
+
+    $(window).trigger('resize');
+
 }
 
 function resizeSlider() {
